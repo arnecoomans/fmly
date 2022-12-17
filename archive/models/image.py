74337b8b2cec1212bc7efba4c2268d5584df8461
Arne Coomans <app+github@arnecoomans.nl>
2022-12-17 07:38:13 +0100

    prepend group title with tag

--- archive/models/image.py
+++ archive/models/image.py
@@ -43,6 +43,14 @@ class Group(models.Model):
   tag                 = models.ManyToManyField(Tag, blank=True, related_name='groups')
   def __str__(self):
     title = self.title
+    ''' Prepend title with tag if there is one tag set. 
+        If more tags are set, it would get too messy. 
+    '''
+    if self.tag and self.tag.all().count() == 1:
+      for tag in self.tag.all():
+        title = f"{ tag }: { title }"
+    ''' Append title with short description if set 
+    '''
     if self.description:
       title += ' (' + self.description[:32]
       if len(self.description) > 32:
