b524d43d9039fc67071029bf9b3dcbcaae44e14e
Arne Coomans <app+github@arnecoomans.nl>
2024-10-24 15:59:07 +0200

    use partial rendering and comments loading via jquery

---
+++ archive/static/js/autoload_comments.js
@@ -0,0 +1,24 @@
+// function getComments(objectid, objectslug){
+//   var url = 'object/' + objectid + '/' + objectslug + '/a/comments/';
+
+// }
+
+function fetchAllComments(url, csrf_token) {
+  console.log('foo');
+  $.ajax({
+    url: url,
+    headers: {'X-CSRFToken': csrf_token},
+    dataType: 'json',
+    success: function(data){
+      // Based on the status of the response, take the correct action
+      if (data.error == true) {
+        $('#comment-messages').append('<div class="alert alert-danger" role="alert">' + data.status.name + ' when loading comments: ' + data.status.message + '</div>');
+        return false;
+      } else {
+        $.each(data['payload'], function(index, comment){
+          $('.autoload.comments').append(comment);
+        });
+      } 
+    }
+  });
+}
