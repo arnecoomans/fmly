678ce7c99114e288c8382b425a0298a0eddc1c7c
Arne Coomans <app+github@arnecoomans.nl>
2025-11-07 09:53:31 +0100

    re-introduce family/urls.py since it is a vital part of the site

---
+++ family/urls.py
@@ -0,0 +1,16 @@
+"""family URL Configuration"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+  path('pick/', include('datepicker.urls')),
+  path('', include('archive.urls')),
+  path('ajax/', include('cmnsd.urls')),
+  path('admin/', admin.site.urls),
+  path('', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
