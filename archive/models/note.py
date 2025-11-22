beda18c382a8137f2d4519155052fd19c295c5cf
Arne Coomans <github@arnecoomans.nl>
2025-11-22 20:04:40 +0100

    use cmsnd basemodel for note

--- archive/models/note.py
+++ archive/models/note.py
@@ -6,15 +6,16 @@ from .person import Person
 from .tag import Tag
 from .image import Image, Attachment
 
+from cmnsd.models.cmnsd_basemodel import BaseModel
 #from django.template.defaultfilters import slugify
 
-class Note(models.Model):
+class Note(BaseModel):
   title               = models.CharField(max_length=255, blank=True)
   content             = models.TextField(blank=True, help_text='Markdown Supported')
   # Meta
-  date_modified       = models.DateTimeField(auto_now=True)
-  date_created        = models.DateTimeField(auto_now_add=True)
-  user                = models.ForeignKey(User, on_delete=models.CASCADE)
+  # date_modified       = models.DateTimeField(auto_now=True)
+  # date_created        = models.DateTimeField(auto_now_add=True)
+  # user                = models.ForeignKey(User, on_delete=models.CASCADE)
   # Relations
   images              = models.ManyToManyField(Image, blank=True, related_name='notes')
   people              = models.ManyToManyField(Person, blank=True, related_name='notes')
