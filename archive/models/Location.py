0e5f16ad8680301afd078fd6aa9b3c85bc4f71be
Arne Coomans <github@arnecoomans.nl>
2025-12-07 11:45:31 +0100

    allow non-unique locations to differientiate between colonial and non-colonial times

--- archive/models/Location.py
+++ archive/models/Location.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.template.defaultfilters import slugify
 from cmnsd.models.cmnsd_basemodel import BaseModel
 from .BaseIcon import BaseIcon
 
 class Location(BaseModel, BaseIcon):
   slug                = models.SlugField(max_length=255, unique=True)
-  name                = models.CharField(max_length=255, unique=True)
+  name                = models.CharField(max_length=255)
   description         = models.TextField(blank=True)
   parent              = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='sublocations')
   alternatives        = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='alternative_to')
@@ -17,13 +17,21 @@ class Location(BaseModel, BaseIcon):
   def __str__(self):
     return self.get_display_name()
   
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      self.slug = slugify(self.name)
+    super().save(*args, **kwargs)
+  
   class Meta:
     ordering = ['name']
+    unique_together = ('parent', 'name')
   
   def get_display_name(self):
     name = self.name
     if self.parent:
       name += f", { self.parent.name }"
+    if self.parent and self.parent.parent:
+      name += f", { self.parent.parent.name }"
     return name
   
   def get_alternatives(self):
